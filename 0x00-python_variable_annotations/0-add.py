e6152b69509937a6666c50c6cf10a814cddbdd7e
abubakrali10 <bakrali175@yahoo.com>
2023-12-08 03:01:54 +0200

    create a function to add two numbers with type annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides basic add function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers.
+    Returns:
+    float: The sum of the two numbers a and b.
+    """
+    return a + b
