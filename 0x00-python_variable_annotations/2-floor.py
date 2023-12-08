3052e2e059cf74548bd7424ecfa1f8b79217d99e
abubakrali10 <bakrali175@yahoo.com>
2023-12-08 03:17:38 +0200

    round down a floating-point number to the nearest integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides floor function.
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Rounds down a floating-point number to the nearest integer.
+    Returns:
+    float: The rounded-down integer value.
+    """
+    return math.floor(n)
