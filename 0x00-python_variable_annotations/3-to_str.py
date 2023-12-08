16013caa9a9ae8de4c848551f9e90a46d87a51e5
abubakrali10 <bakrali175@yahoo.com>
2023-12-08 03:35:03 +0200

    convert a floating-point number to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a function for data type conversion.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to a string.
+    Returns:
+    str: The string representation of the input number.
+    """
+    return str(n)
