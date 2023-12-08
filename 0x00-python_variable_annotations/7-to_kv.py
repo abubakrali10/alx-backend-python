4f1819c45e83824025bd8a40112f4b7ffe574055
abubakrali10 <bakrali175@yahoo.com>
2023-12-08 04:12:37 +0200

    convert a key-value pair

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a key-value pair.
+"""
+
+from typing import Union
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """
+    Converts a key-value pair.
+
+    Args:
+    - k (str): The key.
+    - v (Union[int, float]): The value, which can be an integer or a float.
+
+    Returns:
+    tuple[str, float]: A tuple containing the original key and the square of the value.
+    """
+    return k, v * v
