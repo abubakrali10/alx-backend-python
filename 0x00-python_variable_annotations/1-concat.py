0ec9de6d02304db78a0c3434386c5680a68ea1a9
abubakrali10 <bakrali175@yahoo.com>
2023-12-08 03:09:23 +0200

    concatenate two strings with type annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+String Concatenation Module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
